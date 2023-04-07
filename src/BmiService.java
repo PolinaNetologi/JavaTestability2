205755b6919a3eb818b48af1b82517fc296c8cc0
Polina <pzmujdeckaa@gmail.com>
2023-04-07 21:13:28 +0500

    JavaTestability2

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+
+    public int calculate(double heightInMeters, double weightInKg) {
+        double bmi = weightInKg / heightInMeters / heightInMeters;
+        return (int)bmi;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      BmiService service = new BmiService();
+      double heightInMeters = 1.58;
+      double weightInKg = 50.2;
+      int bmi = service.calculate(heightInMeters, weightInKg);
+
+        System.out.println("Индекс массы вашего тела " + bmi);
     }
 }
